71022e0c2c873a228b1dfa17b2b41e19f3164e4f
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-12-04 20:33:52 -0500

    WIP grammar and AST stuff

---
+++ src/Typemaker.Ast/ITypemakerFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Typemaker.Ast
+{
+	interface ITypemakerFile
+	{
+		string FilePath { get; set; }
+		List<INode> Nodes { get; set; }
+	}
+}
