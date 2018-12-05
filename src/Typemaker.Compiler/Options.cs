72155bc5375b81a4b45a06c66e66639a181325bd
karma <noreply@github.com>
2018-12-05 23:31:10 +0000

    Basic compiler

---
+++ src/Typemaker.Compiler/Options.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Typemaker.Compiler
+{
+
+    public class TypemakerOptions
+    {
+        public static class JsonTree
+        {
+            public class ByondVersion
+            {
+                [JsonProperty("major")]
+                public long Major { get; set; }
+
+                [JsonProperty("minor")]
+                public long Minor { get; set; }
+            }
+
+            public class LinterSettings
+            {
+                [JsonProperty("enforce_tabs")]
+                public bool EnforceTabs { get; set; }
+
+                [JsonProperty("allman_braces")]
+                public bool AllmanBraces { get; set; }
+
+                [JsonProperty("no_operator_overloading")]
+                public bool NoOperatorOverloading { get; set; }
+
+                [JsonProperty("no_single_line_blocks")]
+                public bool NoSingleLineBlocks { get; set; }
+            }
+
+            public class Scripts
+            {
+                [JsonProperty("pre_transpile")]
+                public Script PreTranspile { get; set; }
+
+                [JsonProperty("pre_compile")]
+                public Script PreCompile { get; set; }
+
+                [JsonProperty("post_compile")]
+                public Script PostCompile { get; set; }
+            }
+
+            public class Script
+            {
+                [JsonProperty("windows")]
+                public string Windows { get; set; }
+
+                [JsonProperty("linux")]
+                public string Linux { get; set; }
+            }
+        }
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("code_root")]
+        public string CodeRoot { get; set; }
+
+        [JsonProperty("output_directory")]
+        public string OutputDirectory { get; set; }
+
+        [JsonProperty("byond_version")]
+        public JsonTree.ByondVersion BYONDVersion { get; set; }
+
+        [JsonProperty("debug")]
+        public bool Debug { get; set; }
+
+        [JsonProperty("scripts")]
+        public JsonTree.Scripts ShellScripts { get; set; }
+
+        [JsonProperty("dme")]
+        public string Dme { get; set; }
+
+        [JsonProperty("linter_settings")]
+        public JsonTree.LinterSettings LinterSettings { get; set; }
+
+    }
+    public class CommandLineOptions
+    {
+        [Option('r', "read", Required = true, HelpText = "The TM file to compile.", Default = "test.tm")]
+        public string TmFile { get; set; }
+
+        [Option(HelpText = "Sets the DEBUG preprocessor macro.", Default = null)]
+        public bool? DebugMode { get; set; }
+
+        [Option('v', "verbose", Required = false, Default = false)]
+        public bool Verbose { get; set; }
+    }
+}
--- src/Typemaker.Compiler/Program.cs
+++ src/Typemaker.Compiler/Program.cs
@@ -1,18 +1,52 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.IO;
 
 namespace Typemaker.Compiler
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			using (var fs = new FileStream("../../../test.tm", FileMode.Open, FileAccess.Read))
-			{
-				var lexer = new Parser.TypemakerLexer(new Antlr4.Runtime.AntlrInputStream(fs));
-				var tokens = lexer.GetAllTokens();
-				var i = 0;
-			}
-		}
-	}
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /* TODO
+             * Move this logic elsewhere
+             */
+            CommandLineOptions options = null;
+
+            CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(opt => options = opt).WithNotParsed<CommandLineOptions>(err =>
+            {
+                Console.WriteLine("Error reading command-line arguments. Quitting...");
+                Environment.Exit(-1);
+            }
+            );
+
+            if (!File.Exists("typemaker.json"))
+            {
+                Console.WriteLine("No typemaker.json configuration found. Quitting...");
+                Environment.Exit(-1);
+            }
+
+            TypemakerOptions typemakerOptions = null;
+
+            try
+            {
+                typemakerOptions = Newtonsoft.Json.JsonConvert.DeserializeObject<TypemakerOptions>(File.ReadAllText("typemaker.json"));
+            }
+            catch (Newtonsoft.Json.JsonException e)
+            {
+                Console.WriteLine("Error deserializing typemaker.json. Error message: {0}. Quitting...", e.Message);
+            }
+
+            if (options.DebugMode != null)
+            {
+                typemakerOptions.Debug = (bool)options.DebugMode;
+            }
+
+            using (FileStream fs = new FileStream(options.TmFile, FileMode.Open, FileAccess.Read))
+            {
+                Parser.TypemakerLexer lexer = new Parser.TypemakerLexer(new Antlr4.Runtime.AntlrInputStream(fs));
+                System.Collections.Generic.IList<Antlr4.Runtime.IToken> tokens = lexer.GetAllTokens();
+            }
+        }
+    }
 }
