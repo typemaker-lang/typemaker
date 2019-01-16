4af802cd801a0cd4c8c7455c8c6b271cd5962de0
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-01-16 15:47:51 -0500

    Bracketed expression

---
+++ src/Typemaker.Ast/Statements/Expressions/BracketedExpression.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Typemaker.Ast.Statements.Expressions
+{
+	sealed class BracketedExpression : Expression, IBracketedExpression
+	{
+		public IExpression Interior => ChildAs<IExpression>();
+
+		public override bool HasSideEffects => Interior.HasSideEffects;
+
+		public override RootType? EvaluateType() => Interior.EvaluateType();
+
+		public BracketedExpression(IEnumerable<ITrivia> children) : base(children)
+		{
+
+		}
+	}
+}
---
+++ src/Typemaker.Ast/Statements/Expressions/IBracketedExpression.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Typemaker.Ast.Statements.Expressions
+{
+	interface IBracketedExpression
+	{
+		IExpression Interior { get; }
+	}
+}
